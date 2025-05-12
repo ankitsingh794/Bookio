4d8f3d109525bf3fb4349839e09784244f789bfd
Ankit Singh <ankitsinghrjt794@gmail.com>
2025-05-12 18:59:29 +0530

    Home Page Frontend Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Cards } from "./Cards";
-// import { CardsWrapper } from "./CardsWrapper";
+import { CardsWrapper } from "./CardsWrapper";
 import { Frame } from "./Frame";
 import { Navigation } from "./Navigation";
-// import { NavigationFooter } from "./NavigationFooter";
+import { NavigationFooter } from "./NavigationFooter";
 import { Section } from "./Section";
 import { Text } from "./Text";
 import "./Home.css";
@@ -12,12 +12,16 @@ const Home = () => {
   return (
     <div className="frame-screen">
       <div className="home-wrapper">
+        <Navigation />
         <div className="home">
-          <div className="customer-reviews">Customer Reviews</div>
-
-          <Cards />
-          <NavigationFooter />
-          <Navigation />
+          <div className="group">
+            <div className="page-title">
+              <div className="text-wrapper-26">Featured</div>
+              <p className="text-wrapper-27">
+                Trending Events Right Now: Explore Top Events Near You!
+              </p>
+            </div>
+          </div>
 
           <div className="overlap-group">
             <div className="hero-image" />
@@ -30,13 +34,9 @@ const Home = () => {
           <div className="text-wrapper-22">Discover Events Near You</div>
 
           <Frame />
+
           <div className="image-6" />
-
-          <div className="text-wrapper-23">Upcoming Events</div>
-
-          <CardsWrapper />
           <div className="text-wrapper-24">Join the Community</div>
-
           <Text />
           <div className="buttons-2">
             <button className="button-3">
@@ -48,16 +48,18 @@ const Home = () => {
             </button>
           </div>
 
+          <div className="text-wrapper-23">Upcoming Events</div>
+
+          <Cards />
+
+          <div className="customer-reviews">Customer Reviews</div> 
+          <CardsWrapper />
+
+
           <Section />
-          <div className="group">
-            <div className="page-title">
-              <div className="text-wrapper-26">Featured</div>
-              <p className="text-wrapper-27">
-                Trending Events Right Now: Explore Top Events Near You!
-              </p>
-            </div>
-          </div>
+          
         </div>
+        <NavigationFooter />
       </div>
     </div>
   );
